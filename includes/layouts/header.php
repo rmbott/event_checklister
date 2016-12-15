a16c1cd480ad210e4f7713f3af7897fb2b8bd042
Ryan Bott <ryan@rmbott.com>
2016-12-15 12:17:48 -0500

    Added JQueryUI to page header (header.php) fixing the autocomplete is not a function error.

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/styles.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <title>Event Checklister</title>
