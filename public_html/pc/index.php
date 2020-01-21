3005cf537ce507d6b3de72acc626ddacc2c8bd05
rmbott <ryan@rmbott.com>
2020-01-21 15:12:17 -0500

    Removed some example code from Keith Casey's code from chapter 6 of his Lynda.com course PHP: Design Patterns (https://www.lynda.com/PHP-tutorials/Design-Patterns-PHP/186870-2.html) which served as the base MVC structure.

--- public_html/pc/index.php
+++ public_html/pc/index.php
@@ -1,12 +1,9 @@
 <?php
 
 /**
- * Copyright (c) 2014 Keith Casey
- *
- * This code is designed to accompany the lynda.com video course "Design Patterns in PHP"
- *   by Keith Casey. If you've received this code without seeing the videos, go watch the
- *   videos. It will make way more sense and be more useful in general.
+ * App Landing
  */
+ 
 require_once('../../library/initialize.php');
 
 $action = isset($_GET['a']) ? $_GET['a'] : 'index';
@@ -14,13 +11,12 @@ $module = isset($_GET['m']) ? $_GET['m'] : '';
 $id     = isset($_GET['id']) ? $_GET['id'] : '';
 
 switch($module) {
-    case 'number':
-        $controller = new NumberController();
-        break;
+    //case 'number':
+    //    $controller = new NumberController();
+    //    break;
 	case 'user':
 		$controller = new UserController($session);
 		break;
-		
     default:
         $controller = new DefaultController();
 }
--- views/default.php
+++ views/default.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-But this one has a single entry point and everything else - what to view and how - is determined by the controllers.
 <ul>
     <li>index</li>
-    <li><a href="index.php?a=view&m=number&id=1">one</a></li>
-    <li><a href="index.php?a=view&m=number&id=2">two</a></li>
-    <li><a href="index.php?a=view&m=number&id=3">three</a></li>
-    <li><a href="index.php?a=view&m=number&id=4">four</a></li>
     <li><a href="index.php?a=login&m=user">login</a></li>
 </ul>
 </body>
